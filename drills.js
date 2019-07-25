42d6edcf3e68694ca08e1294b08ff004697d21cb
Lucas Vocos <lucas.vocos@me.com>
2019-07-25 16:10:03 -0400

    Singly linked list

--- drills.js
+++ drills.js
@@ -111,11 +111,6 @@ class LinkedList {
     }
     let newItem = new _Node(item, targetNode);
     previousNode.next = newItem;
-    console.log(
-      `Previous is ${
-        previousNode.value
-      }\n Newly inserted item is ${item}\n and tareget is ${targetNode.value}`
-    );
   }
 
   insertAfter(item, target) {
@@ -143,6 +138,28 @@ class LinkedList {
     let newItem = new _Node(item, targetNode.next);
     targetNode.next = newItem;
   }
+
+
+  insertAt(item, num) {
+    // if it's empty
+    if (!this.head) {
+      console.log('This link list is empty, sorry');
+      return null
+    }
+    let currentNode = this.head
+
+    for (let counter = 1; counter <= num; counter++) {
+      currentNode = currentNode.next
+      if (counter === num) {
+        this.insertBefore(item, currentNode.value)
+      }
+    }
+
+    if (currentNode.value === null) {
+      return null
+    }
+
+  }
 }
 
 function main() {
@@ -164,13 +181,19 @@ function main() {
   SLL.insertLast('Husker');
   SLL.insertLast('Starbuck');
   SLL.insertLast('Tauhida');
-  //SLL.remove('squirrel') // this logs a 'Item not found' and returns null
+  // SLL.remove('squirrel') // this logs a 'Item not found' and returns null
 
   SLL.insertBefore('Athena', 'Boomer');
   SLL.insertAfter('Hotdog', 'Helo');
-  console.log(SLL.find('Apollo'));
-  console.log(SLL.find('Boomer'));
-  console.log(SLL.find('Hotdog'));
-  console.log(SLL.find('Helo'));
+  SLL.insertAt('Kat', 3)
+  // console.log(SLL.find('Boomer'));
+  // console.log(SLL.find('Kat'));
+  console.log('Before removing Tauhida');
+  console.log(SLL.find('Starbuck'));
+  console.log('---------------------------------------------');
+  SLL.remove('Tauhida')
+  console.log('AFTER removing tauhida');
+  console.log(SLL.find('Starbuck'));
+
 }
 main();
